81eaac3928c43160ac1d87dc3379bec68ddd1c66
ruanovski <ruanvic21@gmail.com>
2024-01-19 17:16:43 -0300

    Estilização do body -> Adição da logo github no body -> Criaçao e estilização do input e do button

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,20 @@
 import Header from "../../components/Header";
+import background from "../../assets/background.png";
+import "./style.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="content">
+        <img src={background} alt="github log" className="background" />
+        <div className="info">
+          <div>
+            <input type="text" name="user" placeholder="@username" />
+            <button>Find</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
