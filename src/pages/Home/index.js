bcc07700f09ff9adc7c12e524720fd169b5476c4
ruanovski <ruanvic21@gmail.com>
2024-01-23 12:40:41 -0300

    Estilização da parte repositories na pagina homr -> criaçao do template itemlist

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import Header from "../../components/Header";
 import background from "../../assets/background.png";
+import ItemList from "../../components/ItemList";
 import "./style.css";
 
 function App() {
@@ -12,6 +13,25 @@ function App() {
           <div>
             <input type="text" name="user" placeholder="@username" />
             <button>Find</button>
+          </div>
+          <div className="profile">
+            <img
+              src="https://avatars.githubusercontent.com/u/97916151?s=400&u=65128de805bcc7fb48acf78cad63b92507db5e05&v=4"
+              alt="github user"
+            />
+            <div>
+              <h2>Ruan Victor</h2>
+              <h5>@username</h5>
+              <br />
+              <p>Descrição</p>
+            </div>
+          </div>
+          <hr />
+          <div>
+            <h4 className="RepositoriesHeader">Repositories</h4>
+            <ItemList title="teste" description="descricao" />
+            <ItemList title="teste" description="descricao" />
+            <ItemList title="teste" description="descricao" />
           </div>
         </div>
       </div>
